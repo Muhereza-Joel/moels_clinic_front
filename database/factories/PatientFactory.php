da536ea98de9f6cf42bb175a22ff8a5af43bbc86
Muhereza-Joel <muherezajoel40@gmail.com>
2026-01-23 20:10:36 +0300

    update factory

--- database/factories/PatientFactory.php
+++ database/factories/PatientFactory.php
@@ -10,12 +10,37 @@ class PatientFactory extends Factory
 {
     protected $model = Patient::class;
 
+    /**
+     * Generate MRN with fixed prefix "MOELS-PT"
+     */
+    public static function generateMrn(): string
+    {
+        $orgCode = 'MOELS'; // fixed org prefix
+        $patientPrefix = 'PT-';
+        $suffixLength = 6;
+
+        $max = (36 ** $suffixLength) - 1;
+
+        do {
+            $rand = random_int(0, $max);
+            $base36 = base_convert($rand, 10, 36);
+
+            $suffix = strtoupper(
+                str_pad($base36, $suffixLength, '0', STR_PAD_LEFT)
+            );
+
+            $mrn = "{$orgCode}-{$patientPrefix}{$suffix}";
+        } while (Patient::where('mrn', $mrn)->exists());
+
+        return $mrn;
+    }
+
     public function definition(): array
     {
         return [
             'uuid' => Uuid::uuid4()->toString(),
             'organization_id' => 2, // fixed org ID for your case
-            'mrn' => null, // will be auto-generated in Patient::booted()
+            'mrn' => self::generateMrn(), // call factory’s own MRN generator
             'first_name' => $this->faker->firstName,
             'last_name' => $this->faker->lastName,
             'sex' => $this->faker->randomElement(['Male', 'Female']),
